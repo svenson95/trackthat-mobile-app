8c831e1b5f4ea245b1fc8e3c234940c27f7c9e96
Sven Brodny <svenbrodny@googlemail.com>
2025-08-25 18:07:22 +0200

    chore: add api url in env files

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -2,11 +2,13 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import type { Observable } from 'rxjs';
 
+import { environment } from '../../environments/environment.prod';
+
 import type { User } from '../models';
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
-  private apiUrl = 'https://trackthat-backend.fly.dev/api/users';
+  private apiUrl = environment.api + 'users';
   private http = inject(HttpClient);
 
   getUsers(): Observable<Array<User>> {
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  api: 'https://trackthat-backend.fly.dev/api/',
 };
