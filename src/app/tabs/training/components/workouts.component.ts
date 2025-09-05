6486cb28d1399b3c13bf8eca8f3cdab094c069e5
Sven Brodny <svenbrodny@googlemail.com>
2025-09-05 14:57:06 +0200

    Update deleteWorkout flow (dont toggle edit mode)

--- src/app/tabs/training/components/workouts.component.ts
+++ src/app/tabs/training/components/workouts.component.ts
@@ -116,11 +116,9 @@ export class WorkoutsComponent {
 
     this.service.deleteWorkout(id).subscribe({
       next: () => {
-        this.isEditing.set(false);
         void loading.dismiss();
       },
       error: (err) => {
-        this.isEditing.set(false);
         void loading.dismiss();
         console.error('Unexpected fail during delete user.workoutId', err);
       },
