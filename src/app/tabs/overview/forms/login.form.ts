a256fc52693553ebb95f264bf1a5f2afddeea6cf
Sven Brodny <svenbrodny@googlemail.com>
2025-09-05 14:03:29 +0200

    refactor: login (2)

--- src/app/tabs/overview/forms/login.form.ts
+++ src/app/tabs/overview/forms/login.form.ts
@@ -42,7 +42,7 @@ export class LoginForm implements OnInit {
 
   ngOnInit(): void {
     google.accounts.id.initialize({
-      client_id: process.env['GOOGLE_CLIENT_ID'],
+      client_id: '81384485805-o4b55e424moljjf98egavlhol819l18a.apps.googleusercontent.com',
       callback: (res: GoogleResponse) =>
         this.authService.loginWithGoogleToken(res.credential).subscribe({
           next: (res) => this.authService.login(res),
