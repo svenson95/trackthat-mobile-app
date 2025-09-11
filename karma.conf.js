05f8de1392b6b52543da32ada929a96fb906e10e
Sven Brodny <svenbrodny@googlemail.com>
2025-09-11 16:07:40 +0200

    deploy: fix unit tests (2)

--- karma.conf.js
+++ karma.conf.js
@@ -34,8 +34,14 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
+    browsers: ['ChromeHeadlessNoSandbox'],
+    singleRun: true,
     restartOnFileChange: true,
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-setuid-sandbox'],
+      },
+    },
   });
 };
