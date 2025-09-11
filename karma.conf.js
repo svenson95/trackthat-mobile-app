500968e28279feaa407effd2f0a8d65ef56d90f4
Sven Brodny <svenbrodny@googlemail.com>
2025-09-11 15:49:12 +0200

    Revert "fix(karma): No binary for ChromeHeadless browser on your platform"
    This reverts commit 8b32df51d70a7fe3d35300334f25eb829f72635a.

--- karma.conf.js
+++ karma.conf.js
@@ -1,8 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-process.env.CHROME_BIN = require('puppeteer').executablePath();
-
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -36,14 +34,8 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadlessCustom'],
-    singleRun: true,
+    browsers: ['Chrome'],
+    singleRun: false,
     restartOnFileChange: true,
-    customLaunchers: {
-      ChromeHeadlessCustom: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox', '--disable-gpu'],
-      },
-    },
   });
 };
