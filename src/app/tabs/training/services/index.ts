95e8bc5996f29f0c5738249bc882c3305b1a0a62
Sven Brodny <svenbrodny@googlemail.com>
2026-05-18 23:02:18 +0200

    refactor(training): delete unused resolver

--- src/app/tabs/training/services/index.ts
+++ src/app/tabs/training/services/index.ts
@@ -1,5 +1,3 @@
 export * from './is-editing.service';
-export * from './log-workout-resolver.service';
 export * from './logs-workout.service';
-export * from './workout-resolver.service';
 export * from './workouts.service';
