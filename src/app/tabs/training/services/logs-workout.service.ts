9600ff579c08de7af39947d8332f4572558b3e43
Sven Brodny <svenbrodny@googlemail.com>
2026-05-20 14:33:33 +0200

    feat(log-workout): get log error message in ui

--- src/app/tabs/training/services/logs-workout.service.ts
+++ src/app/tabs/training/services/logs-workout.service.ts
@@ -1,8 +1,7 @@
 import { HttpClient, httpResource } from '@angular/common/http';
-import { computed, inject, Injectable } from '@angular/core';
+import { computed, effect, inject, Injectable } from '@angular/core';
 import { tap, type Observable } from 'rxjs';
 
-import { UserService } from 'src/app/services';
 import { environment } from '../../../../environments/environment.prod';
 import type {
   DeleteLogWorkoutBody,
@@ -11,6 +10,7 @@ import type {
   PostLogWorkoutResponse,
   WorkoutSet,
 } from '../../../models';
+import { ToastService, UserService } from '../../../services';
 
 @Injectable({
   providedIn: 'root',
@@ -19,6 +19,7 @@ export class LogsWorkoutService {
   private readonly apiUrl = environment.api + 'logs-workout';
   private readonly http = inject(HttpClient);
   private readonly userService = inject(UserService);
+  private readonly toastService = inject(ToastService);
 
   readonly logWorkoutResource = httpResource<GetLogWorkoutDTO | null>(() => {
     const date = this.getTodayStartTimestamp();
@@ -34,6 +35,15 @@ export class LogsWorkoutService {
     const logWorkout = this.logWorkoutResource.value();
     return logWorkout?.logId;
   });
+
+  constructor() {
+    effect(async () => {
+      const error = this.logWorkoutResource.error();
+      if (!error) return;
+
+      await this.toastService.show('tabs.training.log-workout.actions.get-error');
+    });
+  }
 
   addLogWorkout(date: number, set: WorkoutSet, userId: string): Observable<PostLogWorkoutResponse> {
     return this.http
