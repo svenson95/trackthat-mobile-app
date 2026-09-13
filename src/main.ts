c6be5739c4246af514e532abf8050dc83440458e
Sven Brodny <svenbrodny@googlemail.com>
2026-09-13 11:27:30 +0200

    chore(deps): update Angular to v21

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
+/* eslint-disable no-console */
+import { provideZoneChangeDetection } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
-// eslint-disable-next-line no-console
-bootstrapApplication(AppComponent, appConfig).catch((err) => console.log(err));
+bootstrapApplication(AppComponent, {
+  ...appConfig,
+  providers: [provideZoneChangeDetection(), ...appConfig.providers],
+}).catch((err) => console.log(err));
